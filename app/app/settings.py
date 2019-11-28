37ca6a8b0107a8dce594160068d3b69bc7ee0f35
hamidrezadavirani <hamidrezadavirani@gmail.com>
2019-11-28 12:13:24 +0330

    this

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# autopep?
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
