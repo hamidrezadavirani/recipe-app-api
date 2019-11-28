bff4de03ddabb9073567bcf4d707d5ee1b79ec63
hamidrezadavirani <hamidrezadavirani@gmail.com>
2019-11-28 12:35:34 +0330

    deathpunch

--- app/manage.py
+++ app/manage.py
@@ -2,7 +2,6 @@
 import os
 import sys
 
-# thissssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
